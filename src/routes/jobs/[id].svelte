37e14dc8ea2b2b4ecd21985b0634f62416b03c24
Kim Hegedus <kim@22ndtech.com>
2020-12-17 10:00:20 -0700

    added error message to jobs page

--- src/routes/jobs/[id].svelte
+++ src/routes/jobs/[id].svelte
@@ -4,13 +4,25 @@
         const { id } = page.params;
 
         const res = await this.fetch(`jobs/${id}.json`);
-        const job = await res.json();
 
-        return { job }
+        if (res.status === 200) {
+            const job = await res.json();
+            return { job }
+        }
+
+        if (res.status === 404) {
+            const { error } = await res.json();
+            this.error(404, error);
+        }
     }
   </script>
 
   <script>
       export let job;
-      console.log(job);
   </script>
+
+ <div class="job">
+     <h2>{ job.title }</h2>
+     <p>Salary of ${ job.salary }</p>
+     <p>{ job.details }</p>
+ </div>
